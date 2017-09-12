ec8cb714a41d966a828a48b0f8cf0c9b87aae3a0
shinyay <shinya.com@gmail.com>
2017-09-12 12:43:10 +0900

    Enhance option handling

--- template-script.sh
+++ template-script.sh
@@ -2,7 +2,8 @@
 
 # ##################################################
 #
-version="1.0.0"
+PROGNAME=$(basename $0)
+VERSION="1.0.0"
 #
 # HISTORY:
 #
@@ -22,8 +23,8 @@ Usage:
     $(basename ${0}) [command] [<options>]
 
 Options:
-    --version, -v     print $(basename ${0}) ${version}
-    --help, -h        print help
+    -v, --version     print $(basename ${0}) ${VERSION}
+    -h, --help        print help
 EOF
 }
 
@@ -34,34 +35,58 @@ if [ $# -eq 0 ]; then
 fi
 
 # Handle Options
-while [ $# -gt 0 ];
+for OPT in "$@"
 do
-    case ${1} in
-
-        --debug|-d)
-            set -x
-        ;;
-
-        --version|-v)
-            echo "$(basename ${0}) ${version}"
-            exit 0
-        ;;
-
-        --host|-h)
-            HOST=${2}
-            shift
-        ;;
-
-        --port|-p)
-            PORT=${2}
-            shift
-        ;;
-
-        *)
-            echo "[ERROR] Invalid option '${1}'"
-            usage
-            exit 1
-        ;;
-    esac
-    shift
+  case "$OPT" in
+    '-v'|'--version' )
+      echo "$(basename ${0}) ${VERSION}"
+      exit 0
+      ;;
+    '-h'|'--help' )
+      usage
+      exit 0
+      ;;
+    '-d'|'--debug' )
+      set -x
+      ;;
+    '-a'|'--aoption' )
+      if [[ -z "$2" ]] || [[ "$2" =~ ^-+ ]]; then
+        echo "$PROGNAME: option requires an argument -- $1" 1>&2
+        exit 1
+      fi
+      ARG_A="$2"
+      shift 2
+      ;;
+    '-b'|'--long-b' )
+      if [[ -z "$2" ]] || [[ "$2" =~ ^-+ ]]; then
+        shift
+      else
+        shift 2
+      fi
+      ;;
+    '-c'|'--long-c' )
+      shift 1
+      ;;
+    '--'|'-' )
+      shift 1
+      param+=( "$@" )
+      break
+      ;;
+    -*)
+      echo "$PROGNAME: illegal option -- '$(echo $1 | sed 's/^-*//')'" 1>&2
+      exit 1
+      ;;
+    *)
+      if [[ ! -z "$1" ]] && [[ ! "$1" =~ ^-+ ]]; then
+        param+=( "$1" )
+        shift 1
+      fi
+      ;;
+  esac
 done
+
+if [ -z $param ]; then
+  echo "$PROGNAME: too few arguments" 1>&2
+  echo "Try '$PROGNAME --help' for more information." 1>&2
+  exit 1
+fi
